9e59f8a56d6ecd391748db6c68c8da8f1219d3f6
Faouzi El Mansour <faouzielmansour@yahoo.com>
2017-03-31 20:48:47 +0100

    mainprincipal modifié

--- src/gestionpersonnel/vue/MainPrincipal.java
+++ src/gestionpersonnel/vue/MainPrincipal.java
@@ -16,12 +16,27 @@ import javax.swing.JFrame;
 public class MainPrincipal {
     private JFrame frame;
     private AfficherLeMenu afficherMenu;
+    private ListeEmploye liste_Employe;
+    private ListeAgence liste_Agence;
+    private EntrerEmploye entre_employe;
+    private EntrerAffectation entre_affectAffectation;
+    private EntrerPermission entre_Permission;
+    private EntrerAgence entre_Agence;
+    
+    
     
     public MainPrincipal(){
         frame = new JFrame("Gestion de Permission et d'Affectation CONGELCAM");
         afficherMenu = new AfficherLeMenu();
+        liste_Employe = new ListeEmploye();
+        liste_Agence = new ListeAgence();
+        entre_Agence = new EntrerAgence();
+        entre_employe = new EntrerEmploye();
+        entre_affectAffectation = new EntrerAffectation();
+        entre_Permission = new EntrerPermission();
         
         afficherMenu.addEmployeListener(new ListeEmployeListener());
+        entre_employe.addValiderInsertListener(new AjouterEmploye());
         
         
         
@@ -38,6 +53,78 @@ public class MainPrincipal {
             frame.setVisible(true);
         }
     }
+    
+    class AjouterEmploye implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            frame.setContentPane(entre_employe);
+        }
+    }
+
+    public JFrame getFrame() {
+        return frame;
+    }
+
+    public void setFrame(JFrame frame) {
+        this.frame = frame;
+    }
+
+    public AfficherLeMenu getAfficherMenu() {
+        return afficherMenu;
+    }
+
+    public void setAfficherMenu(AfficherLeMenu afficherMenu) {
+        this.afficherMenu = afficherMenu;
+    }
+
+    public ListeEmploye getListe_Employe() {
+        return liste_Employe;
+    }
+
+    public void setListe_Employe(ListeEmploye liste_Employe) {
+        this.liste_Employe = liste_Employe;
+    }
+
+    public ListeAgence getListe_Agence() {
+        return liste_Agence;
+    }
+
+    public void setListe_Agence(ListeAgence liste_Agence) {
+        this.liste_Agence = liste_Agence;
+    }
+
+    public EntrerEmploye getEntre_employe() {
+        return entre_employe;
+    }
+
+    public void setEntre_employe(EntrerEmploye entre_employe) {
+        this.entre_employe = entre_employe;
+    }
+
+    public EntrerAffectation getEntre_affectAffectation() {
+        return entre_affectAffectation;
+    }
+
+    public void setEntre_affectAffectation(EntrerAffectation entre_affectAffectation) {
+        this.entre_affectAffectation = entre_affectAffectation;
+    }
+
+    public EntrerPermission getEntre_Permission() {
+        return entre_Permission;
+    }
+
+    public void setEntre_Permission(EntrerPermission entre_Permission) {
+        this.entre_Permission = entre_Permission;
+    }
+
+    public EntrerAgence getEntre_Agence() {
+        return entre_Agence;
+    }
+
+    public void setEntre_Agence(EntrerAgence entre_Agence) {
+        this.entre_Agence = entre_Agence;
+    }
+    
+    
     
     public static void main(String[] args) {
         MainPrincipal m = new MainPrincipal();
